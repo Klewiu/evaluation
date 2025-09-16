c5729fa57da69b24c0b6a6ce6e72202e9f25d510
TomPrint <tomekklewicki@gmail.com>
2025-09-16 22:07:52 +0200

    add userlist and delete functionality with HTMX

--- src/users/urls.py
+++ src/users/urls.py
@@ -3,7 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/',  auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path("", views.users_list, name="users_list"),
+    path('', views.users_list, name='users_list'),
+    path('<int:pk>/confirm-delete/', views.user_confirm_delete, name='user_confirm_delete'),
+    path('<int:pk>/delete/',         views.user_delete,         name='user_delete'),
 ]
